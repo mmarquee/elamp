17169b386cac7698f05a457b7060f8a0f8cebd1a
Mark Humphreys <inpwtepydjuf@gmail.com>
2025-12-26 09:37:37 +0000

    chore: initial dump of stuff

---
+++ src/electron/preload.cts
@@ -0,0 +1,12 @@
+const electron = require('electron');
+
+electron.contextBridge.exposeInMainWorld(
+    "electron", {
+        subscribeStatistics: (callback: (statistics: any) => void ) => {
+            // @ts-ignore
+            electron.ipcRenderer.on("statistics", (_, stats) => {
+                callback({stats});
+        })},
+        getStaticData: () => { console.log("getStaticData")}
+    }
+)
---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,24 @@
+import os from 'os';
+import { BrowserWindow } from 'electron';
+import osUtils from  'os-utils';
+
+const POLLING_INTERVAL = 500;
+
+export const pollResources = (mainWindow: BrowserWindow) => {
+    setInterval (async () => {
+        const cpuUsage = await getCpuUsage()
+        mainWindow.webContents.send("statistics", {cpuUsage})
+    },
+    POLLING_INTERVAL)
+}
+
+const getCpuUsage = () => {
+    return new Promise(resolve => {
+        osUtils.cpuUsage(resolve)
+    })
+}
+
+export const getStaticData = () => {
+    const cpumodel = os.cpus()[0].model;
+    return ({ cpumodel })
+}
