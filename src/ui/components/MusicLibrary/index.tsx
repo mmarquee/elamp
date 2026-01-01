a69e8822aa1ce68c978d657fee8a7199ed55cdee
Mark Humphreys <inpwtepydjuf@gmail.com>
2026-01-01 16:10:54 +0000

    use correct(ish) structure for metadata

--- src/ui/components/MusicLibrary/index.tsx
+++ src/ui/components/MusicLibrary/index.tsx
@@ -50,7 +50,7 @@ export const MusicLibrary = ({ metaData }: Props) => {
               <TableRow 
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
                 <TableCell component="th" scope="row">
-                  {row.common ? row.common.title: "UNKNOWN"}
+                  {row.metadata.common ? row.metadata.common.title: "UNKNOWN"}
                 </TableCell>
               </TableRow>
             ))}
