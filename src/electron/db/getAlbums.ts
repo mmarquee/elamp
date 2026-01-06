b650ba27147bf9864863978688a0461cc74b4f84
Mark Humphreys <inpwtepydjuf@gmail.com>
2026-01-06 20:33:31 +0000

    feat: first kludge of 'proper' data

--- src/electron/db/getAlbums.ts
+++ src/electron/db/getAlbums.ts
@@ -1,13 +1,19 @@
 import { data } from "./index.js";
-import { unique } from "./utils.js";
 
 export const getAlbums = (): AlbumsData => {
-  console.log(data.length);
-  const result = data.map((item) => `${item.metadata?.common.artist}|${item.metadata?.common.album}|${item.metadata?.common.year}`).filter(unique);
-
-  console.log({
-    albums: result,
-  });
+  const result = Array.from(
+    new Set(
+      data
+        .map((item) => {
+          return {
+            artist: item.metadata?.common.artist,
+            album: item.metadata?.common.album,
+            year: item.metadata?.common.year,
+          };
+        })
+        .map((obj) => JSON.stringify(obj))
+    )
+  ).map((e) => JSON.parse(e));
 
   return {
     albums: result,
